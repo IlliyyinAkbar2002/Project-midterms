18f0eb91d6f3eefe6fe3b3be0c2dbf9143e87f83
dimashfirayhan <dimasryhn23@gmail.com>
2025-04-30 07:56:47 +0700

    Add and Replacing Function

---
+++ Project_C#/Back_end/State.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_C_.Back_end
+{
+    public enum State
+    {
+        Login,
+        Register,
+        MainMenu,
+        Logout,
+        Home
+    }
+}
---
+++ Project_C#/Back_end/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_C_.Back_end
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+
+        public User(string username, string password, Role role)
+        {
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+    }
+}
